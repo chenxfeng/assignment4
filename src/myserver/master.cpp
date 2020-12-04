26d43dc724fd7cc9e629cd4397cab89113851194
chenxfeng <chenxf47@mail2.sysu.edu.cn>
2020-12-04 16:00:45 +0800

    handle multiple pending client requests

--- src/myserver/master.cpp
+++ src/myserver/master.cpp
@@ -19,7 +19,8 @@ static struct Master_state {
   int next_tag;
 
   Worker_handle my_worker;
-  Client_handle waiting_client;
+  ///handle multiple pending client requests
+  map<int, Client_handle> waiting_client;///tag2client
 
 } mstate;
 
@@ -73,9 +74,12 @@ void handle_worker_response(Worker_handle worker_handle, const Response_msg& res
 
   DLOG(INFO) << "Master received a response from a worker: [" << resp.get_tag() << ":" << resp.get_response() << "]" << std::endl;
 
-  send_client_response(mstate.waiting_client, resp);
+  int tag = resp.get_tag();
+  Client_handle client = mstate.waiting_client[tag];
+  send_client_response(client, resp);
 
-  mstate.num_pending_client_requests = 0;
+  mstate.num_pending_client_requests --;
+  mstate.waiting_client.erase(mstate.waiting_client.find(tag));
 }
 
 void handle_client_request(Client_handle client_handle, const Request_msg& client_req) {
@@ -92,28 +96,18 @@ void handle_client_request(Client_handle client_handle, const Request_msg& clien
     return;
   }
 
-  // The provided starter code cannot handle multiple pending client
-  // requests.  The server returns an error message, and the checker
-  // will mark the response as "incorrect"
-  if (mstate.num_pending_client_requests > 0) {
-    Response_msg resp(0);
-    resp.set_response("Oh no! This server cannot handle multiple outstanding requests!");
-    send_client_response(client_handle, resp);
-    return;
-  }
-
-  // Save off the handle to the client that is expecting a response.
-  // The master needs to do this it can response to this client later
-  // when 'handle_worker_response' is called.
-  mstate.waiting_client = client_handle;
-  mstate.num_pending_client_requests++;
-
   // Fire off the request to the worker.  Eventually the worker will
   // respond, and your 'handle_worker_response' event handler will be
   // called to forward the worker's response back to the server.
   int tag = mstate.next_tag++;
   Request_msg worker_req(tag, client_req);
   send_request_to_worker(mstate.my_worker, worker_req);
+
+  // Save off the handle to the client that is expecting a response.
+  // The master needs to do this it can response to this client later
+  // when 'handle_worker_response' is called.
+  mstate.waiting_client[tag] = client_handle;
+  mstate.num_pending_client_requests++;
 
   // We're done!  This event handler now returns, and the master
   // process calls another one of your handlers when action is
