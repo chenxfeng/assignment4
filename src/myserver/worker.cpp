ff7d1ed51198cda9abe402615829e74168620e22
chenxfeng <chenxf47@mail2.sysu.edu.cn>
2020-12-05 12:20:59 +0800

    worker use multiple hyper-threads

--- src/myserver/worker.cpp
+++ src/myserver/worker.cpp
@@ -54,7 +54,7 @@ typedef struct {
 } WorkerArgs;
 
 static struct Worker_state {
-  const static int max_num_tasks = 19;///num_of_thread-1
+  const static int max_num_tasks = 39;///num_of_thread-1
   WorkQueue<Request_msg> block_queue_tasks;
   pthread_t thread_pool[max_num_tasks];
   WorkerArgs thread_arg[max_num_tasks];
